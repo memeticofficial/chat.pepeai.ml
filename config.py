45dd786d9bb8661993df6db112ef9a1a945e56c4
cghost <cghost>
2023-05-16 08:49:29 -0400

    pepeai testnet update

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ import torch
 from petals.constants import PUBLIC_INITIAL_PEERS
 
 
-INITIAL_PEERS = PUBLIC_INITIAL_PEERS
-# Set this to a list of multiaddrs to connect to a private swarm instead of the public one, for example:
-# INITIAL_PEERS = ['/ip4/10.1.2.3/tcp/31234/p2p/QmcXhze98AcgGQDDYna23s4Jho96n8wkwLJv78vxtFNq44']
+INITIAL_PEERS = ['/ip4/54.164.52.79/tcp/8989/p2p/QmR7G6GZzeQeJoXfTovp8PJdJ7qxKYFwXUa2fp9p6BYdDi']
 
 MODEL_NAMES = ["bigscience/bloom-petals", "bigscience/bloomz-petals", "bigscience/bloom-7b1-petals"]
-DEFAULT_MODEL_NAME = "bigscience/bloom-petals"
+DEFAULT_MODEL_NAME = "bigscience/bloomz-petals"
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 TORCH_DTYPE = torch.bfloat16
